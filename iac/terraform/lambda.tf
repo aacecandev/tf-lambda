f0b40936c70e97d63171b163c0a048a6b4e7a55c
Alejandro Aceituna Cano <aacecan.dev@gmail.com>
2022-07-10 16:04:55 +0200

    fix(terrafom): Fix module source

--- iac/terraform/lambda.tf
+++ iac/terraform/lambda.tf
@@ -1,6 +1,6 @@
 module "lambda_function" {
   source        = "terraform-aws-modules/lambda/aws"
-  version       = "~> 3.3.1"
+  version       = "3.3.1"
   function_name = "hello-node"
   description   = "Hello world with NodeJS"
   handler       = "index.handler"
