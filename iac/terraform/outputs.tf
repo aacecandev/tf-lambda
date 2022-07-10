d73835b420317719f654cdb16d42bc52f7ff22e3
Alejandro Aceituna Cano <aacecan.dev@gmail.com>
2022-07-10 19:10:20 +0200

    fix(terrafom): Output S3 bucket id

--- iac/terraform/outputs.tf
+++ iac/terraform/outputs.tf
@@ -5,12 +5,17 @@
 #   value       = aws_s3_bucket.lambda_bucket.bucket
 # }
 
-output "function_name" {
-  description = "Name of the Lambda function"
-  value       = module.lambda_function.lambda_function_name
-}
+# output "function_name" {
+#   description = "Name of the Lambda function"
+#   value       = module.lambda_function.lambda_function_name
+# }
 
 # output "base_url" {
 #   description = "Base URL for API Gateway"
 #   value       = aws_apigatewayv2_stage.lambda.invoke_url
 # }
+
+output "bucket_name" {
+  description = "Name of the S3 bucket used to store function code"
+  value       = aws_s3_bucket.bucket.id
+}
