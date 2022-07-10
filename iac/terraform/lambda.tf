09a0209a6aa74234c4cc8b7c62276c000738dd8c
Alejandro Aceituna Cano <aacecan.dev@gmail.com>
2022-07-10 18:50:13 +0200

    chore(): Temporary stop using modules and deploy only a S3 bucket

--- iac/terraform/lambda.tf
+++ iac/terraform/lambda.tf
@@ -1,14 +1,14 @@
-module "lambda_function" {
-  source        = "terraform-aws-modules/lambda/aws"
-  version       = "3.3.1"
-  function_name = "hello-node"
-  description   = "Hello world with NodeJS"
-  handler       = "index.handler"
-  runtime       = "nodejs12.x"
+# module "lambda_function" {
+#   source        = "terraform-aws-modules/lambda/aws"
+#   version       = "3.3.1"
+#   function_name = "hello-node"
+#   description   = "Hello world with NodeJS"
+#   handler       = "index.handler"
+#   runtime       = "nodejs12.x"
 
-  source_path = "./hello-world/index.js"
+#   source_path = "./hello-world/index.js"
 
-  tags = {
-    Name = "my-lambda1"
-  }
-}
+#   tags = {
+#     Name = "my-lambda1"
+#   }
+# }
